e8d66f214059f8422798ed54fd51f45c2978e01d
Joakim Winsgård <Joakim.w@live.se>
2015-05-25 15:18:32 +0200

    Words

--- ScreenSwing/src/se/mah/k3/Word.java
+++ ScreenSwing/src/se/mah/k3/Word.java
@@ -1,8 +1,7 @@
 package se.mah.k3;
 
 import java.awt.Color;
-import se.mah.k3.particles.FrameParticle;
-import se.mah.k3.particles.TextParticle;
+
 
 //This is the class for the word object. It contains the words that
 //the user will have displayed in their mobile app. 
@@ -13,7 +12,7 @@ public class Word {
 	private String type="";
 	public boolean active = true, selected;
 	public String ownerId = "";
-	public User owner;
+
 	public enum State {onTray, draging, placed,locked};
 	State state=State.onTray;
 	public String text = "";
@@ -31,152 +30,7 @@ public class Word {
 		tyPos=_ty;
 		this.text = _text;
 		this.ownerId = _ownerId;
-		this.active = false;
 	}
-	
-	public void setUser(User _u){ 
-		if(!owner.equals(_u)){
-			owner=_u;
-			ownerId=_u.getId();
-		}
-	}
-	
-	public User getUser(){
-		return owner;
-		
-	}
-
-	public String getText(){
-		return text;
-	}
-
-	public void setXPos(int _xPos){
-		this.xPos = _xPos;
-	}
-
-	public int getXPos(){
-		return xPos;
-	}
-
-	public void setYPos(int _yPos){
-		this.yPos = _yPos;
-	}
-
-	public int getYPos(){
-		return yPos;
-	}
-
-	public void setWidth(int _width){
-		this.width = _width;
-	}
-
-	public int getWidth(){
-		return width;
-	}
-
-	public void setHeight(int _height){
-		this.height = _height;
-	}
-
-	public int getHeight(){
-		return height;
-	}
-
-	public void setText(String _text){
-		this.text = _text;
-	}
-
-	public boolean isActive(){
-		return active;
-	}
-	
-	public void setOwner(String _ownerId){
-		this.ownerId = _ownerId;
-		for(User u: DrawPanel.userList){
-			if(u.getId().equals(this.getOwner())){
-				owner=u;
-			System.out.println("owner setted");
-			}
-		}
-	}
-
-	public String getOwner(){
-		return ownerId;
-	}
-
-	public void respond(){
-		DrawPanel.overParticles.add(new FrameParticle(xPos, yPos, this, 0));
-	}
-
-	public void appear(){
-		DrawPanel.overParticles.add(new TextParticle(xPos, yPos, width, height, margin, 0, text));
-		DrawPanel.overParticles.add(new TextParticle(xPos, yPos, width, height, (- margin), 0, text));
-		DrawPanel.overParticles.add(new TextParticle(xPos, yPos, width, height, (int) (margin * 0.5), 0, text));
-		DrawPanel.overParticles.add(new TextParticle(xPos, yPos, width, height, (int) (- margin * 0.5), 0, text));
-		active=true;
-	}
-
-	public void disappear(){
-		DrawPanel.overParticles.add(new TextParticle(xPos, yPos, width, height, 0, margin, text));
-		DrawPanel.overParticles.add(new TextParticle(xPos, yPos, width, height, 0, (int) (margin * 0.5), text));
-		DrawPanel.overParticles.add(new TextParticle(xPos, yPos, width, height, 0, (int) (margin * 0.2), text));
-		DrawPanel.overParticles.add(new TextParticle(xPos, yPos, width, height, 0, 0, text));
-		DrawPanel.overParticles.add(new TextParticle(xPos, yPos, width, height, 0, (int) (- margin * 0.2), text));
-		DrawPanel.overParticles.add(new TextParticle(xPos, yPos, width, height, 0, (int) (- margin * 0.5), text));
-		DrawPanel.overParticles.add(new TextParticle(xPos, yPos, width, height, 0, (- margin), text));
-
-		active=false;
-	}
-
-	public void selected(){
-		DrawPanel.overParticles.add( new FrameParticle(xPos, yPos, this, 0));
-	}
-
-	public void released(){
-		DrawPanel.overParticles.add( new FrameParticle(xPos, yPos, this));
-	}
-
-	public void display() {
-		
-
-		if ( owner!=null) {
-			//DrawPanel.g2.setColor(Constants.wordStroke);
-			DrawPanel.g2.setColor(owner.getColor());
-		}else {
-			DrawPanel.g2.setColor(Constants.wordStroke);
-		}
-		DrawPanel.g2.fillRect((int) (xPos + 3 - (width * 0.5)) - margin, (int) (yPos + 3 - (height * 0.5) - margin * 0.5), width + margin * 2, height + 6);
-		DrawPanel.g2.setColor(Color.white);
-		DrawPanel.g2.setFont(Constants.lightFont);
-		DrawPanel.g2.drawString(text, (int) (xPos - width * 0.5),(int) (yPos + height* 0.25));
-		
-		if(state==State.draging){
-			DrawPanel.g2.setStroke(Constants.wordOutline);
-			DrawPanel.g2.drawRect((int) (xPos + 3 - (width * 0.5)) - margin, (int) (yPos + 3 - (height * 0.5) - margin * 0.5), width + margin * 2, height + 6);
-		}
-		
-	}
-
-	public void update() {
-		float xDiff=txPos-xPos;
-		float yDiff=tyPos-yPos;
-		//txPos=Math.cos(angle);
-		//tyPos=Math.sin(angle);
-		xPos+=(int)(xDiff*0.2);
-		yPos+=(int)(yDiff*0.2);
-		pxPos=xPos;
-		pyPos=yPos;
-		
-	}
-
-	public void setState(State _state){
-		this.state = _state;
-	}
-
-	public int getState(){
-		return state.ordinal();
-	}	
-
 
 	
 }
--- ScreenSwing/src/se/mah/k3/WordFiles.java
+++ ScreenSwing/src/se/mah/k3/WordFiles.java
@@ -1,20 +1,51 @@
 package se.mah.k3;
 
 import java.io.*;
+import java.util.ArrayList;
 import java.util.Scanner;
 
+
+import com.firebase.client.ChildEventListener;
+import com.firebase.client.DataSnapshot;
+import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
+
+
 public class WordFiles {
-	
+	public static ArrayList<Word> words=new ArrayList <Word>();
 	public static void main (String[] args) throws IOException
 	{
 		
+		Firebase myFirebaseRef, regularWordsRef, themedWordsRef;
+		
+		myFirebaseRef = new Firebase("https://scorching-fire-1846.firebaseio.com/Joakim Words"); // Root
+		//regularWordsRef = new Firebase("https://scorching-fire-1846.firebaseio.com/regularWords");
+		//themedWordsRef = new Firebase("https://scorching-fire-1846.firebaseio.com/themedWords");
+		//myFirebaseRef.removeValue(); // Cleans out everything
+		
+		//Firebase word = myFirebaseRef.child("Joakim Words");
+		
+		myFirebaseRef.child("Themed Wierd Size").setValue(1);
 		
 		Scanner input = new Scanner(new File("Resources/regularWords.txt"));
 		String rWords = input.nextLine();
 		rWords = rWords.toLowerCase();
-		String word[] = rWords.split(" ");
+		String word1[] = rWords.split(" ");
 		System.out.println(rWords);
-		System.out.println(word.length);
+		System.out.println(word1.length);
+		//Firebase wordList = myFirebaseRef.child("Joakim Words");
+		int count = 0;
+		for (int i = 0; i < word1.length; i++) {
+			myFirebaseRef.child("word" + i + "/text").setValue(word1[i]);
+			myFirebaseRef.child("word" + i + "/Active").setValue(false);
+			myFirebaseRef.child("word" + i + "/Owner").setValue("");
+			//int x=r.nextInt(Constants.screenWidth + 1); // skalad x pos
+			//int y=r.nextInt(Constants.screenHeight + 1); // skalad y pos
+			words.add(new Word(word1[i],"hej"));
+			count++;
+		}
+
+		myFirebaseRef.child("Joakim Words Size").setValue(count);
 		
 }
 }

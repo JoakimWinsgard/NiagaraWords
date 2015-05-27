7a5036154816f85487cc64a48fa5aacc8faefb0b
Joakim Winsgård <Joakim.w@live.se>
2015-05-27 17:36:13 +0200

    Word generating program

--- NiagaraWords/src/Monitor.java
+++ NiagaraWords/src/Monitor.java
@@ -4,6 +4,14 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import javax.swing.JTextPane;
+import javax.swing.JScrollPane;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.JComboBox;
+import javax.swing.DefaultComboBoxModel;
 
 
 public class Monitor extends JFrame {
@@ -34,11 +42,39 @@ public class Monitor extends JFrame {
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
 		
-		WordFiles wordFilesnew =new WordFiles();
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setBounds(12, 40, 189, 165);
+		contentPane.add(scrollPane);
+		
+		JTextPane textPane = new JTextPane();
+		scrollPane.setRowHeaderView(textPane);
+		
+		final WordFiles wordFiles = new WordFiles();
+		//Words Words = new Words();
+
+		final JComboBox comboBox = new JComboBox();
+		comboBox.setModel(new DefaultComboBoxModel(new String[] {"regular", "used", "Star Wars"}));
+		comboBox.setBounds(213, 85, 186, 22);
+		contentPane.add(comboBox);
+		
+		JButton btnStart = new JButton("Start");
+		btnStart.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				wordFiles.firebase(comboBox.getSelectedItem().toString());
+			}
+		});
+		btnStart.setBounds(302, 40, 97, 25);
+		contentPane.add(btnStart);
+		
+		JButton btnExit = new JButton("Exit");
+		//btnExit.setBounds(302, 78, 97, 25);
+		contentPane.add(btnExit);
+		
+
+		
 		
 	}
-
 }
--- NiagaraWords/src/WordFiles.java
+++ NiagaraWords/src/WordFiles.java
@@ -6,6 +6,9 @@ import java.util.Scanner;
 
 
 
+
+//import se.mah.k3.Word;
+
 import com.firebase.client.ChildEventListener;
 import com.firebase.client.DataSnapshot;
 import com.firebase.client.Firebase;
@@ -14,17 +17,23 @@ import com.firebase.client.FirebaseError;
 
 public class WordFiles {
 	public static ArrayList<Word> words=new ArrayList <Word>();
-	public static Firebase myFirebaseRef = new Firebase("https://scorching-fire-1846.firebaseio.com/Joakim Words"); // Root
-	//regularWordsRef = new Firebase("https://scorching-fire-1846.firebaseio.com/regularWords");
-	//themedWordsRef = new Firebase("https://scorching-fire-1846.firebaseio.com/themedWords");
-	//myFirebaseRef.removeValue(); // Cleans out everything
+	//public ArrayList<String> list= new ArrayList<String>();
+	public Firebase myFirebaseRef ;
+	public static String word1[],word2[]; 
 	
-	//Firebase word = myFirebaseRef.child("Joakim Words");
-	
-		WordFiles(){
+	 WordFiles (){
 		
-		myFirebaseRef.setValue("test");
-		myFirebaseRef.removeValue();
+		//Firebase myFirebaseRef, regularWordsRef, themedWordsRef;
+		
+		myFirebaseRef = new Firebase("https://scorching-fire-1846.firebaseio.com/Joakim Words"); // Root
+		//regularWordsRef = new Firebase("https://scorching-fire-1846.firebaseio.com/regularWords");
+		//themedWordsRef = new Firebase("https://scorching-fire-1846.firebaseio.com/themedWords");
+		//myFirebaseRef.removeValue(); // Cleans out everything
+		
+		//Firebase word = myFirebaseRef.child("Joakim Words");
+		
+		myFirebaseRef.child("Themed Wierd Size").setValue(1);
+		
 		Scanner input = null;
 		try {
 			input = new Scanner(new File("Resources/regularWords.txt"));
@@ -32,14 +41,32 @@ public class WordFiles {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		String rWords = input.nextLine();
-		rWords = rWords.toLowerCase();
-		String word1[] = rWords.split(" ");
-		System.out.println(rWords);
-		System.out.println(word1.length);
+		Scanner input2 = null;
+		try {
+			input2 = new Scanner(new File("Resources/StarWarsTheme.txt"));
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	
+		word1 = input.nextLine().toLowerCase().split(" ");
+		word2 = input2.nextLine().toLowerCase().split(" ");
+
+		System.out.println("loadar in words");
 		//Firebase wordList = myFirebaseRef.child("Joakim Words");
+		
+}
+
+	public void firebase(String s){
+		
+		System.out.println(s + "  stuff" );
+		
+		
 		int count = 0;
+		System.out.println(word1.length+"hejsan");
+		System.out.println(word1.length);
 		for (int i = 0; i < word1.length; i++) {
+	
 			myFirebaseRef.child("word" + i + "/text").setValue(word1[i]);
 			myFirebaseRef.child("word" + i + "/Active").setValue(false);
 			myFirebaseRef.child("word" + i + "/Owner").setValue("");
@@ -48,8 +75,45 @@ public class WordFiles {
 			words.add(new Word(word1[i],"hej"));
 			count++;
 		}
-
-		myFirebaseRef.child("Joakim Words Size").setValue(count);
 		
- }
+		myFirebaseRef.child("Joakim Words Size").setValue(count);
+		System.out.println("regular is on firebase now!!");
+		
+		
+	switch(s){
+			
+			case "Star Wars":
+				
+				count = 0;
+	
+				System.out.println("Star Wars theme is on firebase now!!");
+				System.out.println(word2.length);
+				for (int i = 0; i < word2.length; i++) {
+			
+					myFirebaseRef.child("word" + i + "/text").setValue(word2[i]);
+					myFirebaseRef.child("word" + i + "/Active").setValue(false);
+					myFirebaseRef.child("word" + i + "/Owner").setValue("");
+					//int x=r.nextInt(Constants.screenWidth + 1); // skalad x pos
+					//int y=r.nextInt(Constants.screenHeight + 1); // skalad y pos
+					words.add(new Word(word2[i],"hej"));
+					count++;
+				}
+				myFirebaseRef.child("second Size").setValue(count);
+				
+				
+			break;
+			
+			case "used":
+			
+			break;
+	
+	}
+	
+	
+	
+	}
+	
+	
+	
+	
 }
